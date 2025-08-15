5de660990e4ffdab642191318b67b7ecc5ad223b
Artur Voll <artur.voll@gmail.com>
2025-08-15 08:00:44 +0200

    updated UserDefinedSimplefieldAnonymousPHPType with value handling, constructor, and string representation

--- src/Classes/UserDefinedSimplefield/UserDefinedSimplefieldAnonymousPHPType.php
+++ src/Classes/UserDefinedSimplefield/UserDefinedSimplefieldAnonymousPHPType.php
@@ -9,9 +9,48 @@ class UserDefinedSimplefieldAnonymousPHPType
 {
 
     /**
+     * @var mixed $__value
+     */
+    private $__value = null;
+
+    /**
      * @var string $feldname
      */
     private $feldname = null;
+
+    /**
+     * Construct
+     *
+     * @param mixed $value
+     */
+    public function __construct($value)
+    {
+        $this->value($value);
+    }
+
+    /**
+     * Gets or sets the inner value
+     *
+     * @param mixed $value
+     * @return mixed
+     */
+    public function value()
+    {
+        if ($args = func_get_args()) {
+            $this->__value = $args[0];
+        }
+        return $this->__value;
+    }
+
+    /**
+     * Gets a string value
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return strval($this->__value);
+    }
 
     /**
      * Gets as feldname
